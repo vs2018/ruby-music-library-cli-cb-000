3c0c0a54c9b95f1ea646f79185372824002cc428
Vishal Sharma <vsh1819@gmail.com>
2018-07-06 16:45:04 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -14,7 +14,7 @@ class MusicLibraryController
     
     input = ""
     
-    while input != exit
+    # while input != exit
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
     puts "To list all of the artists in your library, enter 'list artists'."
@@ -28,9 +28,9 @@ class MusicLibraryController
       input = gets
     end
     end
-      if input == "list songs"
-    self.list_songs
-  end
+  #     if input == "list songs"
+  #   self.list_songs
+  # end
   end
 
   
